22e540044615347035211d707d870b94715a5e3d
Elul-Asfaha <69459338+Elul-Asfaha@users.noreply.github.com>
2023-05-26 13:44:42 +0300

    Add files via upload

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,11 @@
 const Projects = () => {
     return (
-        <div className='px-[10%] py-[5%] min-h-[80vh] flex flex-col items-center gap-5 bg-blue-700'>
-            <div className='flex flex-col md:grid md:grid-cols-2 gap-5'>
-                <p className='text-white text-5xl capitalize'>
+        <div className='px-[10%] py-[5%] min-h-[80vh] bg-blue-700'>
+            <div className='flex flex-col md:grid md:grid-cols-2 gap-5 w-full pb-5'>
+                <p className='text-white text-5xl md:text-7xl capitalize'>
                     take a look at my projects
                 </p>
-                <p className='text-gray-300 p-5'>
+                <p className='text-gray-200 p-5 max-w-md'>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
                     officiis veritatis iure dolore voluptates commodi!
                 </p>
@@ -64,7 +64,6 @@ const Projects = () => {
                     <div></div>
                 </div>
             </div>
-            <button className='w-fit  py-2 px-4'>Show all</button>
         </div>
     );
 };
